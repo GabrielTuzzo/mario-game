40fdf1e86afb0cdb42af34ae90042c55b094e78b
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2023-02-15 23:30:05 +0000

    new commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 document.addEventListener("keydown", function jump (e) {
     mario.classList.add('jump')
@@ -11,7 +12,25 @@ document.addEventListener("keydown", function jump (e) {
 const loop = setInterval(() => {
     
 const pipePosition = document.querySelector('.pipe').offsetLeft
-console.log(pipePosition)
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+console.log(marioPosition)
+
+
+if (pipePosition <= 260 && pipePosition > 20 && marioPosition <= 50) {
+    pipe.style.animation = 'none'
+    pipe.style.left = `${pipePosition}px`
+
+    mario.style.animation = 'none'
+    mario.style.bottom = `${marioPosition}px`
+
+    mario.src = 'imgs/mario-game-over.png'
+    mario.style.width = '130px'
+    mario.style.marginBottom = '100px'
+    mario.style.marginLeft = '120px'
+
+    clearInterval(loop)
+
+}
 
 
 }, 10)
