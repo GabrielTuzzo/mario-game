7206d878dba4817fd458e5aff351c7f72c72a348
Gabriel Tuzzo <76889705+GabrielTuzzo@users.noreply.github.com>
2023-02-16 14:50:06 +0000

    new commit

--- js/index.js
+++ js/index.js
@@ -1,13 +1,61 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const text = document.querySelector('.text');
+const content = document.querySelector('.content');
 
-document.addEventListener("keydown", function jump (e) {
+document.addEventListener("keydown", function jump () {
     mario.classList.add('jump')
+    pipe.style.animation = 'pipe-animation 2s infinite ease-out'
+    content.style.opacity = '1'
+    text.innerHTML = ''
 
     setTimeout(() => {
         mario.classList.remove('jump')
-    }, 500)
+    }, 510)
 })  
+document.addEventListener("keydown", function jump2 () {
+    mario.classList.add('jump2')
+
+    setTimeout(() => {
+        mario.classList.remove('jump2')
+    }, 550)
+})  
+document.addEventListener("keydown", function jump3 () {
+    mario.classList.add('jump3')
+
+    setTimeout(() => {
+        mario.classList.remove('jump3')
+    }, 580)
+})  
+document.addEventListener("keydown", function jump4 () {
+    mario.classList.add('jump4')
+
+    setTimeout(() => {
+        mario.classList.remove('jump4')
+    }, 620)
+})  
+document.addEventListener("keydown", function jump5 () {
+    mario.classList.add('jump5')
+
+    setTimeout(() => {
+        mario.classList.remove('jump5')
+    }, 680)
+})  
+document.addEventListener("keydown", function jump6 () {
+    mario.classList.add('jump6')
+
+    setTimeout(() => {
+        mario.classList.remove('jump6')
+    }, 750)
+})  
+document.addEventListener("keydown", function jump7 () {
+    mario.classList.add('jump7')
+
+    setTimeout(() => {
+        mario.classList.remove('jump7')
+    }, 1000)
+})  
+
 
 const loop = setInterval(() => {
     
@@ -27,10 +75,19 @@ if (pipePosition <= 260 && pipePosition > 20 && marioPosition <= 50) {
     mario.style.width = '130px'
     mario.style.marginBottom = '100px'
     mario.style.marginLeft = '120px'
+    text.innerHTML = 'Oops!'
+    text.style.weight = 'bold'
+    text.style.fontSize = '100px'
+    
+
 
     clearInterval(loop)
 
+    setTimeout(() => {
+        location.reload()
+    }, 300)
 }
 
 
 }, 10)
+
